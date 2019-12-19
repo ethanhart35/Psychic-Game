b1f7755cbdbe465face2bb5e0d7e1a5e9e9f20fc
Ethan Hart <ethanhart35@gmail.com>
2019-12-19 12:33:39 -0700

    built basic HTML, started javascript, added reset code into reset.css

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+
+
+
+//letter array that holds the whole alphabet
+var letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+for (var i = 0; i < letter.length; i++) {
+    document.getElementById("")
+}
